60cf8819f6a4d70ca286ff0f2757ffab31549f4c
SurvivalistRedo <SurvivalistRedo@outlook.com>
2022-10-12 10:50:00 -0700

    strip

--- Line2D.gd
+++ Line2D.gd
@@ -7,10 +7,10 @@ func _process(delta):
 	points[points.size()-1] = get_global_mouse_position() #Vector2(rand_range(250,750),rand_range(250,750))
 	for i in range(100):
 		for n in range(points.size()-2,-1,-1):
-			points[n] = normalizeDist(points[n],points[n+1],100)
+			points[n] = normalizeDist(points[n],points[n+1],5)
 	for i in range(100):
 		for n in range(1,points.size(),1):
-			points[n] = normalizeDist(points[n],points[n-1],100)
+			points[n] = normalizeDist(points[n],points[n-1],5)
 
 func normalizeDist(MovingVector,TargetVector,dist):
 	var angle = 0
